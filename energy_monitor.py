304ae9ac2401404f4a332cd46f9713b3392ef05e
HobbyEd <edwinvandillen@gmail.com>
2020-06-14 13:44:18 +0200

    paramater besturing toegevoegd aan EnergieMonitor

--- energy_monitor.py
+++ energy_monitor.py
@@ -12,7 +12,18 @@ plugwise_smile_host = config_data['plugwise_smile']['host']
 plugwise_smile_password = config_data['plugwise_smile']['password']
 
 persistor = plugwise_persistor.PersistTimeSeriesForPlugwiseSmile(influxdb_host,influxdb_database, influxdb_user,influxdb_password,plugwise_smile_host,plugwise_smile_password)
-while True: 
-    persistor.persist_plugwise_smile_actueel()
-    time.sleep(2)
 
+def actueel_data(): 
+    while True: 
+        persistor.persist_plugwise_smile_actueel()
+        time.sleep(2)
+
+def cumulatief_data(): 
+    while True: 
+        persistor.persist_plugwise_smile_cumulatief()
+        time.sleep(1800)
+
+if __name__ == "actueel":
+    actueel_data()
+elif __name__ == "cumulatief": 
+    cumulatief_data()
